6cc83c63fbfb261bc65702efe2eb37002b66e2c6
AnnaMatveenko-qa <annamatveenko85@gmail.com>
2024-01-21 15:26:25 +0200

    add test

--- src/test/java/FiltersTest.java
+++ src/test/java/FiltersTest.java
@@ -10,31 +10,31 @@ import java.time.Duration;
 
 
 public class FiltersTest extends BaseTest {
-  /*  @DataProvider(name = "Data")
-    public Object[] checkTitle() {
-        return new Object[]{
-                // "Наявність в місті",
-                "Стан товару"
-                // "Виробник"
+    @DataProvider(name = "Data")
+    public Object[][] checkTitle() {
+        return new Object[][]{
+                {"Наявність в місті", 0},
+                {"Стан товару", 1},
+                {"Виробник", 2}
 
         };
-    }*/
+    }
 
- /*   @Test(dataProvider = "Data")
-    public void checkTitleFilters(String data) {
-        RefrigeratorsPage refrigeratorsPage = new RefrigeratorsPage(driver);
-        Assert.assertTrue(refrigeratorsPage.getProducer().isDisplayed());
-        //  isPresentFilter(data,refrigeratorsPage.createListElements()));
-        System.out.println(refrigeratorsPage.getProducer().getText());
-    }*/
+    @Test(dataProvider = "Data")
+    public void checkTitleFilters(String data, int index) {
+        MainPage mainPage = new MainPage(driver);
+        String actual = mainPage.getTitleFiltersText(index);
+        Assert.assertEquals(actual.toLowerCase(), data.toLowerCase());
+
+    }
 
     @Test
     public void filterAvailabilityCity() {
         MainPage mainPage = new MainPage(driver);
-        String chooseCity = mainPage.putCheckboxCityName(0).getChooseFilter().getText().toLowerCase();
-        ProductPage productPage  =  mainPage.chooseProductPage(0);
+        String chooseCity = mainPage.putCheckboxCityName(1).getChooseFilter().getText().toLowerCase();
+        ProductPage productPage = mainPage.chooseProductPage(1);
         String city = productPage.getValueInputCity().getAttribute("value").toLowerCase();
-       Assert.assertTrue(chooseCity.contains(city));
+        Assert.assertTrue(chooseCity.contains(city));
     }
 
     @Test
@@ -42,18 +42,20 @@ public class FiltersTest extends BaseTest {
         String expected = "Уцінка";
         MainPage mainPage = new MainPage(driver);
         mainPage.putCheckProductCondition();
-        boolean result = mainPage
-                .getTitleProduct(0).contains(expected.toLowerCase());
-        Assert.assertTrue(result);
+        Assert.assertTrue(mainPage.getTitleProduct(1).contains(expected.toLowerCase()));
+
     }
 
     @Test
     public void filterProducer() {
-
+        MainPage mainPage = new MainPage(driver);
+        mainPage.putCheckboxProducerName(0);
+        Assert.assertTrue(mainPage.compareSelectFilterAndProductTitle(0));
     }
 
     @Test
     public void filterPriceByNumber() {
+        MainPage mainPage = new MainPage(driver);
 
     }
 
