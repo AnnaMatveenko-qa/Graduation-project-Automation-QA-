7f31fe123b4fdd65bbf1184777524ebd86cf0b48
AnnaMatveenko-qa <annamatveenko85@gmail.com>
2024-01-22 18:08:21 +0200

    corrected 22.01.2024

--- src/main/java/org/example/pages/ProductPage.java
+++ src/main/java/org/example/pages/ProductPage.java
@@ -1,21 +1,32 @@
 package org.example.pages;
 
 import lombok.Getter;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 @Getter
-public class ProductPage extends BasePage{
+public class ProductPage extends BasePage {
 
     @FindBy(xpath = "(//input[@class='eldo-input'])[1]")
     private WebElement valueInputCity;
-
+    @FindBy(xpath = "//div[@class='product-name']")
+    private WebElement mainTitle;
 
     public ProductPage(WebDriver driver) {
         super(driver);
     }
 
-
-
+    public String chooseNameCityFromFieldInput() {
+        new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.presenceOfElementLocated(By.
+                        xpath("//input[@class='eldo-input']")));
+       return valueInputCity.getAttribute("value").toLowerCase();
+    }
 
 }
