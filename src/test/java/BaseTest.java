4328f5d451e4a5d8d5f2b4148ef19652b38ca354
AnnaMatveenko-qa <annamatveenko85@gmail.com>
2024-02-03 14:10:20 +0200

    corrected

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,5 +1,8 @@
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -11,24 +14,23 @@ import java.time.Duration;
 
 public abstract class BaseTest {
     protected WebDriver driver;
-
-   @BeforeSuite
+    @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
 
 
     @BeforeMethod
-    public void InitDriver() {
-        ChromeOptions options = new ChromeOptions();
+    public void InitDriver() throws RuntimeException{
+       ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         options.addArguments("--remote-debugging-pipe");
         options.addArguments("--headless");
         options.addArguments("--disable-dev-shm-usage");
         driver = new ChromeDriver(options);
-       // driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(7));
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
         driver.get("https://eldorado.ua/uk/holodilniki/c1061560/");
 
 
