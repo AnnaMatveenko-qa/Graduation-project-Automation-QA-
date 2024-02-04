7ccf0b8efe088e2f07bf7c5da8ac191a88e4cba7
AnnaMatveenko-qa <annamatveenko85@gmail.com>
2024-02-04 23:13:46 +0200

    corrected

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,10 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.*;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.BeforeSuite;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 import java.time.Duration;
@@ -16,18 +14,22 @@ import java.time.Duration;
 public abstract class BaseTest {
     protected WebDriver driver;
 
+    @BeforeSuite
+    public void setupClass() {
+        WebDriverManager.chromedriver().clearDriverCache().setup();
+        WebDriverManager.chromedriver().clearResolutionCache().setup();
+    }
+
     @BeforeMethod
     public void InitDriver() throws RuntimeException {
-        WebDriverManager.chromedriver().clearDriverCache().setup();
-       WebDriverManager.chromedriver().clearResolutionCache().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         options.addArguments("--remote-debugging-pipe");
         //  options.addArguments("--windows-size=1920,1080");
         options.addArguments("--disable-dev-shm-usage");
-       // options.addArguments("--headless");
-         driver = new ChromeDriver(options);
-        driver.manage().window().setSize(new Dimension(1600, 900));
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+       // driver.manage().window().setSize(new Dimension(1600, 900));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
         driver.get("https://eldorado.ua/uk/holodilniki/c1061560/");
@@ -38,7 +40,11 @@ public abstract class BaseTest {
     @AfterMethod
     public void TearDown() throws RuntimeException {
         driver.quit();
-        WebDriverManager.chromedriver().quit();
+
     }
 
+    @AfterSuite
+    public void tearDownClass() {
+        WebDriverManager.chromedriver().quit();
+    }
 }
