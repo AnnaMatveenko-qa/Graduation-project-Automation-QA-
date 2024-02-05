83cc2bc2125df304e56f5c66f75a955987674752
AnnaMatveenko-qa <annamatveenko85@gmail.com>
2024-02-05 14:11:47 +0200

    corrected

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -22,14 +22,14 @@ public abstract class BaseTest {
 
     @BeforeMethod
     public void InitDriver() throws RuntimeException {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox");
-        options.addArguments("--remote-debugging-pipe");
+        //ChromeOptions options = new ChromeOptions();
+      //  options.addArguments("--no-sandbox");
+       // options.addArguments("--remote-debugging-pipe");
         //  options.addArguments("--windows-size=1920,1080");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-       // driver.manage().window().setSize(new Dimension(1600, 900));
+        //options.addArguments("--disable-dev-shm-usage");
+        //options.addArguments("--headless");
+        driver = new ChromeDriver();
+        driver.manage().window().setSize(new Dimension(1600, 900));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
         driver.get("https://eldorado.ua/uk/holodilniki/c1061560/");
