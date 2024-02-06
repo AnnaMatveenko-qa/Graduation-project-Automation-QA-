35d2898ece749dc9fa79c2ce33f1ff487409462f
AnnaMatveenko-qa <annamatveenko85@gmail.com>
2024-02-06 18:18:32 +0200

    corrected

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -15,7 +15,7 @@ public abstract class BaseTest {
     protected WebDriver driver;
 
     @BeforeSuite
-    public  void setupClass() {
+    public void setupClass() {
         WebDriverManager.chromedriver().clearDriverCache().setup();
         WebDriverManager.chromedriver().clearResolutionCache().setup();
     }
@@ -24,17 +24,17 @@ public abstract class BaseTest {
     public void InitDriver() throws RuntimeException {
         ChromeOptions options = new ChromeOptions();
 
-       //options.addArguments("--no-sandbox");
+        options.addArguments("--no-sandbox");
         options.addArguments("--remote-debugging-pipe");
-       // options.addArguments("--windows-size=1920,1080");
-        //options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--windows-size=1920,1080");
+        options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--headless=new");
-      //  options.addArguments("--single-process");
+        options.addArguments("--single-process");
 
         driver = new ChromeDriver(options);
-       driver.manage().window().setSize(new Dimension(1600, 900));
+       // driver.manage().window().setSize(new Dimension(1600, 900));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(240));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
         driver.get("https://eldorado.ua/uk/holodilniki/c1061560/");
 
 
@@ -46,7 +46,7 @@ public abstract class BaseTest {
 
     }
 
-   @AfterSuite
+    @AfterSuite
     public void tearDownClass() {
         WebDriverManager.chromedriver().quit();
     }
