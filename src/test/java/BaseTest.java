9b881dd9f0b0eb8ba6359e179f4ff9c4af4963ee
AnnaMatveenko-qa <annamatveenko85@gmail.com>
2024-02-06 15:49:53 +0200

    corrected

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -28,13 +28,13 @@ public abstract class BaseTest {
         options.addArguments("--remote-debugging-pipe");
        // options.addArguments("--windows-size=1920,1080");
         //options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--headless=new");
+       // options.addArguments("--headless=new");
       //  options.addArguments("--single-process");
 
         driver = new ChromeDriver(options);
        driver.manage().window().setSize(new Dimension(1600, 900));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(120));
         driver.get("https://eldorado.ua/uk/holodilniki/c1061560/");
 
 
