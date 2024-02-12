e07540527df06dbf7604afe96d92446719a221bb
AnnaMatveenko-qa <annamatveenko85@gmail.com>
2024-02-12 12:14:00 +0200

    corrected

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -2,18 +2,22 @@ import io.qameta.allure.Allure;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestContext;
+import org.testng.ITestNGMethod;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.time.Duration;
+import java.util.Arrays;
+
 
 
 public abstract class BaseTest {
     protected WebDriver driver;
-    protected final int NUMBER_OF_STARTS = 10;
+    protected final int NUMBER_OF_STARTS = 1;
+
 
     @BeforeSuite
     public void setupClass() {
@@ -25,7 +29,7 @@ public abstract class BaseTest {
     public void deleteFileIfExists() {
         String projectDirectory = System.getProperty("src/main/resources");
         String fileName = "ProductWithStatusAndName.txt";
-        File file = new File(projectDirectory + File.separator + fileName);
+       File file = new File(projectDirectory + File.separator + fileName);
         if (file.exists()) {
             boolean deleted = file.delete();
             if (!deleted) {
@@ -40,8 +44,8 @@ public abstract class BaseTest {
         System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
         this.driver = new ChromeDriver();
         driver.manage().window().setSize(new Dimension(1600, 900));
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
         driver.get("https://eldorado.ua/uk/holodilniki/c1061560/");
     }
 
@@ -55,20 +59,29 @@ public abstract class BaseTest {
         if (driver != null) {
             driver.manage().deleteAllCookies();
             driver.quit();
+       }
+        ITestContext context = result.getTestContext();
+        ITestNGMethod[] testMethods = context.getAllTestMethods();
+        for (ITestNGMethod testMethod : testMethods) {
+            if (Arrays.asList(testMethod.getGroups()).contains("product")) {
+                attachFileToAllureReport();
+            }
         }
     }
-     private  byte[] takeScreenshot(ITestResult result) {
-         try {
-             setBrowserZoom(driver,80);
-             File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-             byte[] screenshotBytes = FileUtils.readFileToByteArray(screenshot);
-             saveScreenshot(screenshot, result.getName() + "_screenshot.png");
-             return screenshotBytes;
-         } catch (IOException e) {
-             e.printStackTrace();
-         }
-         return new byte[0];
-     }
+
+
+    private byte[] takeScreenshot(ITestResult result) {
+        try {
+            setBrowserZoom(driver, 80);
+            File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            byte[] screenshotBytes = FileUtils.readFileToByteArray(screenshot);
+            saveScreenshot(screenshot, result.getName() + "_screenshot.png");
+            return screenshotBytes;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new byte[0];
+    }
 
     private void saveScreenshot(File screenshot, String fileName) {
         try {
@@ -84,12 +97,25 @@ public abstract class BaseTest {
         }
     }
 
+
+    private void attachFileToAllureReport() {
+        String directoryPath = "src/main/resources";
+        String fileName = "ProductWithStatusAndName.txt";
+        String filePath = directoryPath + File.separator + fileName;
+        try {
+            FileInputStream inputStream = new FileInputStream(filePath);
+            Allure.addAttachment("Additional Data", "text/plain", inputStream, "txt");
+        } catch (IOException e) {
+            System.out.println("Error while reading file: " + e.getMessage());
+        }
+    }
+
     private void setBrowserZoom(WebDriver driver, double zoomLevel) {
         String script = "document.body.style.zoom='" + zoomLevel + "%'";
         ((JavascriptExecutor) driver).executeScript(script);
     }
 
-   private void deleteScreenshotIfExists(ITestResult result) {
+    private void deleteScreenshotIfExists(ITestResult result) {
         String directory = "src/main/resources/";
         String fileName = result.getName() + "_screenshot.png";
         File screenshotFile = new File(directory + fileName);
